87548edaa0085d5ea30153325d2f7da922964bd1
danielbinschmid <danielbinschmid@outlook.de>
2023-01-31 12:55:21 +0100

    removed comments in a test file

--- src/test/java/com/function/FunctionTest.java
+++ src/test/java/com/function/FunctionTest.java
@@ -54,31 +54,5 @@ public class FunctionTest {
         // Verify
         assertEquals(ret.getStatus(), HttpStatus.OK);
 
-        
-        
-        // String leaseID = leaseClient.acquireLease(10);
-        // System.out.println("obtained lease with id: " + leaseID);
-        // SharedAccessSignature=sv=2021-10-04&ss=btqf&srt=sco&st=2023-01-29T22%3A20%3A01Z&se=2023-02-10T22%3A20%3A00Z&sp=rwdxftlacup&sig=aikwt6G1xIRGPaw8h6K%2F9v5qFg0qTuI5xm37K0A2IUs%3D;BlobEndpoint=https://testhttpscript.blob.core.windows.net/;FileEndpoint=https://testhttpscript.file.core.windows.net/;QueueEndpoint=https://testhttpscript.queue.core.windows.net/;TableEndpoint=https://testhttpscript.table.core.windows.net/;
-        // String h = "here";
-        // BinaryData data = BinaryData.fromBytes(h.getBytes());
-        // blobClient.upload(data);;
-
-        // DefaultAzureCredential defaultCredential = new DefaultAzureCredentialBuilder().build();
-        //     
-        // // Azure SDK client builders accept the credential as a parameter
-        //BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        //        .endpoint("https://testhttpscript.blob.core.windows.net/")
-        //         .credential(defaultCredential)
-        //        .buildClient(); // https://testhttpscript.blob.core.windows.net/tasks/yaya.txt
-        // BlobContainerClient client = blobServiceClient.getBlobContainerClient("tasks"q);
-        //         
-        // BlobClient blobClient = client.getBlobClient("a");
-        //     
-        // BlobLeaseClient leaseClient = new BlobLeaseClientBuilder()
-        //                 .blobClient(blobClient)
-        //                 .buildClient();
-        //         
-        // String leaseID = leaseClient.acquireLease(10);
-        // System.out.println("obtained lease with id: " + leaseID);
     }
 }
