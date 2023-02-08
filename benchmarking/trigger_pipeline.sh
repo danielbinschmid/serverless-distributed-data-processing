1f7fe525f32575e321169c15255af65452ec899c
Ermias Habtegabr <habtegabrermias@gmail.com>
2023-02-08 16:55:57 +0100

    benchmarking

--- benchmarking/trigger_pipeline.sh
+++ benchmarking/trigger_pipeline.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-# USAGE: API-URL <pipeline_type> <filelist-json (file.name)>
 
-curl -d @$3 -X POST $1$2 -H "Content-Type: application/json" 
+echo "triggering" $2 "pipeline"
 
+# the output from 'curl' command is discarded remove '> /dev/null' to see the output
+trigger_with_multiple_batches() {
+  json_files="./json_files/filelist_batch[0-9]*.json"
+  echo -e "multiple batches start time:\n" $(date +%H:%M:%S)
+
+  for json_file in $json_files
+  do
+    #filename="$(basename -- $json_file)"
+
+    echo "triggering with file" $json_file
+    curl -X POST $1"/"$2 \
+         -H "Content-Type: application/json" \
+         -d @$json_file > /dev/null &
+    done
+}
+
+trigger_with_single_batch() {
+  echo -e "single batches start time:\n" $(date +%H:%M:%S)
+
+  filename="./json_files/filelist.json"
+  echo "triggering with file" $filename
+  curl -X POST $1"/"$2 \
+       -H "Content-Type: application/json" \
+       -d @$filename > /dev/null
+}
+
+trigger_with_multiple_batches "$@"
+#trigger_with_single_batch "$@"
