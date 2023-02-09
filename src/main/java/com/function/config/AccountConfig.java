581d5aa2a1b99541860d22e2b1fd1aabd1fd64ae
danielbinschmid <danielbinschmid@outlook.de>
2023-02-09 04:20:50 +0100

    -

--- src/main/java/com/function/config/AccountConfig.java
+++ src/main/java/com/function/config/AccountConfig.java
@@ -2,11 +2,11 @@ package com.function.config;
 
 public class AccountConfig {
 
-    public static final String STORAGE_ACC_NAME = "cbdpqueuec7faf4";
-    public static final String STORAGE_ACC_PRIM_KEY = "SlzkntSPPlxIoIV3rhYcfVpnJl3QSO7ja6P5M7FjUQZfXVmQXNxuIxhsp0owzceSAi2szjfQpgwB+AStKfLXUA==";
+    public static final String STORAGE_ACC_NAME = "<your_storage_acc_name>";
+    public static final String STORAGE_ACC_PRIM_KEY = "<your_storage_acc_primary_key>";
 
     // SAS token for blob-access of storage account.
-    public static final String STORAGE_ACC_SAS_TOKEN = "?sv=2021-10-04&ss=btqf&srt=sco&st=2023-02-09T01%3A49%3A18Z&se=2023-02-10T01%3A49%3A18Z&sp=rwdxftlacup&sig=KOaqiFNesvtlwrsXqUfunv5c1zUtaKhGNSXdpUXp%2FUI%3D";
+    public static final String STORAGE_ACC_SAS_TOKEN = "<your_storage_acc_sas_token>";
     
     // ----------------------- Storage account access configuration strings. --------------------------
 
